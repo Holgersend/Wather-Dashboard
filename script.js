0d6da42c365808a8590b8a87545b0bbc68ba68d6
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-07-11 22:13:14 -0500

    added script
    added the geocoding API to get the coordinates for the city

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-
-
  // Assigning a unique API to a variable
- var APIKey = "daba15fa77e133f3d21659d7e16d88aa";
+const API_KEY = "daba15fa77e133f3d21659d7e16d88aa";
 
- 
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if(!cityName) return;
+
+    console.log(cityName)
+
+    const GEOCODING_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}";
+
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data =>{
+        console.log(data)
+    
+    }).catch(() => {
+        alert("error ocurred while fetching the cordinates");
+    });
+}
+
+searchButton.addEventListener("click", getCityCoordinates); 
