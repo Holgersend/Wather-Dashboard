847cc4221ff9feb36cf48b5a3dcc1257492629f6
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-07-12 22:55:25 -0500

    update js
    fetch the WEATHER API URL (lat lon )
    to get a. unique 5 day forecast array

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const API_KEY = "daba15fa77e133f3d21659d7e16d88aa";
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 
+const getWeatherDetails = (cityName, lat, lon) => {
+    const WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data =>{
+        console.log(data);
+
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if(!uniqueForecastDays.includes(forecastDate)){
+                return uniqueForecastDays.push(forecastDate);
+            }
+        });
+        console.log(fiveDaysForecast);
+    });
+}
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
     if(!cityName) return;
@@ -14,7 +30,8 @@ const getCityCoordinates = () => {
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data =>{
         console.log(data)
-    
+        const { name, lat, lon} = data[0];
+        getWeatherDetails(name, lat, lon);
     }).catch(() => {
         alert("error ocurred while fetching the cordinates");
     });
