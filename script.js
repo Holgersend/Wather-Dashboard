d0a79f49780b8163e8c3e8897097f4dded8d9b93
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-07-12 22:33:58 -0500

    update js
    added the fetch for geocoding API to get the lat and lon data

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const getCityCoordinates = () => {
 
     console.log(cityName)
 
-    const GEOCODING_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}";
+    const GEOCODING_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + API_KEY;
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data =>{
         console.log(data)
