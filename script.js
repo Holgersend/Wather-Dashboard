3809d26ea05ced7612cb816a37b65bcea5a5d537
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-11-14 02:48:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,81 @@ searchBtn.addEventListener('click', function () {
         getWeatherData(cityName);
     }
 });
+// Function to fetch weather data from the OpenWeatherMap API
+function getWeatherData(city) {
+    // Use the OpenWeatherMap API to get current weather data
+    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+    // Use the OpenWeatherMap API to get 5-day forecast data
+    const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
+
+    // Fetch current weather data
+    fetch(currentWeatherURL)
+        .then(response => response.json())
+        .then(data => {
+            // Update the current weather section
+            updateCurrentWeather(data);
+        });
+
+    // Fetch 5-day forecast data
+    fetch(forecastURL)
+        .then(response => response.json())
+        .then(data => {
+            // Update the 5-day forecast section
+            updateForecast(data);
+        });
+}
+
+// Function to update the current weather section
+function updateCurrentWeather(data) {
+    // Extract relevant data from the API response
+    const city = data.name;
+    const date = new Date(data.dt * 1000).toLocaleDateString();
+    const temperature = data.main.temp;
+    const windSpeed = data.wind.speed;
+    const humidity = data.main.humidity;
+
+    // Update the HTML content
+    currentWeather.innerHTML = `
+        <div class="details">
+            <h2>${city} (${date})</h2>
+            <h4>Temperature: ${temperature} K</h4>
+            <h4>Wind: ${windSpeed} m/s</h4>
+            <h4>Humidity: ${humidity}%</h4>
+        </div>
+    `;
+}
+
+// Function to update the 5-day forecast section
+function updateForecast(data) {
+    // Clear existing forecast cards
+    forecastList.innerHTML = '';
+
+    // Extract relevant data from the API response
+    const forecastData = data.list;
+
+    // Loop through the forecast data for the next 5 days
+    for (let i = 0; i < forecastData.length; i += 8) {
+        const forecast = forecastData[i];
+        const date = new Date(forecast.dt * 1000).toLocaleDateString();
+        const temperature = forecast.main.temp;
+        const windSpeed = forecast.wind.speed;
+        const humidity = forecast.main.humidity;
+
+        // Create a new forecast card and append it to the list
+        const forecastCard = document.createElement('li');
+        forecastCard.classList.add('card');
+        forecastCard.innerHTML = `
+            <h4>${date}</h4>
+            <h4>Temperature: ${temperature} K</h4>
+            <h4>Wind: ${windSpeed} m/s</h4>
+            <h4>Humidity: ${humidity}%</h4>
+        `;
+        forecastList.appendChild(forecastCard);
+    }
+}
+
+
+
+
+
